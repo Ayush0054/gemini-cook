33a733ced01977dfda35b3dbda703fd2832e9c08
Ayush0054 <ayush5aprill@gmail.com>
2023-12-16 02:44:53 +0530

    chal gaya code

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,15 +1,47 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
+import { Chat } from "openai/resources/index.mjs";
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+
+// ...
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+  },
+];
+
 const genAI = new GoogleGenerativeAI(process.env.API_KEY as string);
-export async function GET(req: Request, res: NextResponse) {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+export async function POST(req: Request, res: NextResponse) {
+  try {
+    const { history, msg } = await req.json();
 
-  const prompt = "Write a story about a magic backpack.";
+    console.log("Received history:", history);
+    console.log("Received msg:", msg);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-pro",
+      safetySettings,
+    });
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  console.log(response.text());
+    const chat = model.startChat({
+      history,
+      generationConfig: {
+        maxOutputTokens: 100,
+        temperature: 0.9,
+        topP: 0.1,
+        topK: 16,
+      },
+    });
 
-  const text = response.text();
-  return NextResponse.json({ text }, { status: 200 });
+    const result = await await chat.sendMessage(msg);
+    const response = await result.response;
+    console.log(response.text());
+
+    const text = response.text();
+    return NextResponse.json({ text }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ err }, { status: 500 });
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,50 +4,108 @@ import { Avatar, Button, Card, Input, Textarea } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { ScrollShadow } from "@nextui-org/react";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
+import axios from "axios";
+
 export default function Home() {
   const [messages, setMessages] = useState();
-  const output = async () => {
-    const response = await fetch("/api", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+  const [inputMessage, setInputMessage] = useState("");
 
-      // body: JSON.stringify({ messages }),
+  const [chatHistory, setChatHistory] = useState([
+    {
+      role: "user",
+      parts: ["Hello, I have 2 dogs in my house."],
+    },
+    {
+      role: "model",
+      parts: "Great to meet you. What would you like to know?",
+    },
+  ]);
+  useEffect(() => {
+    setChatHistory([
+      {
+        role: "user",
+        parts: "Hello, I have 2 dogs in my house.",
+      },
+      {
+        role: "model",
+        parts: "Great to meet you. What would you like to know?",
+      },
+    ]);
+  }, []);
+  const sendMessage = async () => {
+    const emptyUserMessage = { role: "user", parts: "" };
+
+    const newHistory = [
+      ...chatHistory,
+      { role: "user", parts: inputMessage },
+      { role: "model", parts: "" },
+    ];
+    setChatHistory(newHistory);
+    console.log("Sending request:", { history: newHistory, msg: inputMessage });
+    console.log(inputMessage);
+    console.log(newHistory);
+
+    const response = await axios.post("/api", {
+      history: newHistory,
+      msg: inputMessage,
     });
-    const data = await response.json();
+    console.log(response);
+
+    const data = await response.data;
+
     console.log(data);
 
     setMessages(data.text);
+    // const updatedHistory = [...newHistory, { role: "model", parts: data.text }];
+    const updatedHistory = [...newHistory];
+    updatedHistory[newHistory.length - 1] = { role: "model", parts: data.text };
+
+    setChatHistory(updatedHistory);
   };
-  useEffect(() => {
-    output();
-  }, []);
+
   return (
-    <div className=" flex bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ">
-      <main className="flex min-h-screen flex-col items-center justify-between md:px-24 py-5  bg-black/40 ">
-        <div className="flex md:w-[73%] w-[95%] md:p-3 p-2 flex-col items-center ">
+    <div className="  bg-gradient-to-l from-rose-100 to-teal-100  ">
+      <main className="flex min-h-screen flex-col items-center justify-between md:px-24 py-5    ">
+        <div className="flex md:w-[73%] w-[95%] md:p-3 p-2 flex-col items-center  ">
           {/* <div className=" h-full flex flex-col gap-2 overflow-y-auto bg-scroll  py-8 px-3 w-full"> */}
           <div>
             <h1 className=" text-lg font-semibold text-black">Bot</h1>
 
-            <ScrollShadow
-              hideScrollBar
-              offset={100}
-              orientation="horizontal"
-              className=" min-h-[730px] max-h-[730px] "
-            >
-              <h1 className=" md:text-base text-xs text-white">{messages}</h1>
-            </ScrollShadow>
+            <div className=" min-h-[730px] max-h-[730px] ">
+              {chatHistory.map((message, index) => (
+                <div key={index}>
+                  <h5>{message.role} </h5>
+                  <ScrollShadow
+                    offset={100}
+                    orientation="horizontal"
+                    className="  max-h-[730px] "
+                  >
+                    <h1 className=" md:text-lg font-medium  text-xs text-gray-700">
+                      {" "}
+                      {message.parts}{" "}
+                    </h1>
+                  </ScrollShadow>
+                </div>
+              ))}
+            </div>
           </div>
           {/* </div> */}
           <div className="relative  md:w-[100%] w-[95%] bottom-4 gap-2 flex justify-center items-center ">
             <Textarea
+              type="text"
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.target.value)}
               variant="underlined"
+              color="success"
               className="
           resize-none overflow-y-auto text-black   rounded-l outline-none"
             />
-            <Button className="py-2" variant="">
+            <Button
+              className="py-2"
+              variant="ghost"
+              color="success"
+              onClick={sendMessage}
+            >
               send
             </Button>
           </div>
